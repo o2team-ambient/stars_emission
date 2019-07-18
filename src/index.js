52d4b97d132369adf4c932e5896ee1147e6e5a98
Lee EC <lyxuncle@gmail.com>
2019-07-18 16:17:57 +0800

    feat: 增加上报

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ try {
     if (!window[O2_AMBIENT_CONFIG]) return
     clearInterval(csi)
     initAmbient()
+    const _mtac = {}
+    (function() {
+        const mta = document.createElement("script")
+        mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4"
+        mta.setAttribute("name", "MTAH5")
+        mta.setAttribute("sid", "500690671")
+        const s = document.getElementsByTagName("script")[0]
+        s.parentNode.insertBefore(mta, s)
+    })()
   }, 1000)
 } catch (e) {
   console.log(e)
