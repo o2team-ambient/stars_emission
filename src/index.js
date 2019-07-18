6a47eb38b8e0402b407b58f8cdd82b2cd7391966
Lee EC <lyxuncle@gmail.com>
2019-07-18 16:30:47 +0800

    feat: 修复上报脚本

--- src/index.js
+++ src/index.js
@@ -4,21 +4,23 @@ import './css/package.scss'
 import { O2_AMBIENT_CONFIG } from './js/utils/const'
 import initAmbient from './js/ambient'
 
+const _mtac = {}
+
 try {
   // 保证配置读取顺序
   let csi = setInterval(() => {
     if (!window[O2_AMBIENT_CONFIG]) return
     clearInterval(csi)
     initAmbient()
-    const _mtac = {}
-    (function() {
+    const dataReport = () => {
         const mta = document.createElement("script")
         mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4"
         mta.setAttribute("name", "MTAH5")
         mta.setAttribute("sid", "500690671")
         const s = document.getElementsByTagName("script")[0]
         s.parentNode.insertBefore(mta, s)
-    })()
+    }
+    dataReport()
   }, 1000)
 } catch (e) {
   console.log(e)
