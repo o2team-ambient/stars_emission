11bd6d7161c082e4faee40bb5598c4610f8dfb27
Lee EC <lyxuncle@gmail.com>
2019-08-14 21:15:19 +0800

    feat: 增加上报

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './css/base.scss'
 import './css/package.scss'
 
+import Report from '@o2team/ambient-report'
+
 import { O2_AMBIENT_CONFIG } from './js/utils/const'
 import initAmbient from './js/ambient'
-
-const _mtac = {}
 
 try {
   // 保证配置读取顺序
@@ -12,16 +12,10 @@ try {
     if (!window[O2_AMBIENT_CONFIG]) return
     clearInterval(csi)
     initAmbient()
-    const dataReport = () => {
-        const mta = document.createElement("script")
-        mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4"
-        mta.setAttribute("name", "MTAH5")
-        mta.setAttribute("sid", "500690671")
-        const s = document.getElementsByTagName("script")[0]
-        s.parentNode.insertBefore(mta, s)
-    }
-    dataReport()
   }, 1000)
 } catch (e) {
   console.log(e)
 }
+
+Report.init({})
+Report.processPV()
